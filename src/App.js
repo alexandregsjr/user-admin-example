5acef77fb9633a0ff8adb2d2e4b0e0235ae79187
Alexandre Severo Jr <alexandre.severo@me.com>
2019-07-31 01:04:13 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import NavigationBar from './NavigationBar';
+import MainTable from "./MainTable";
+
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <NavigationBar/>
+        <MainTable/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
